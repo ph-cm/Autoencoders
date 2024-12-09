27d927310a700751d4b61f9745a7294db38c14cf
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-12-09 17:20:50 -0300

    Training some samples

--- Autoencoder.py
+++ Autoencoder.py
@@ -13,4 +13,48 @@ def plotn(n,x):
         ax[i].imshow(z.reshape(28,28) if z.size==28*28 else z.reshape(14,14) if z.size==14*14 else z)
     plt.show()
     
-plotn(10,x_train)
+plotn(5,x_train)
+
+#Training some samples
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Lambda
+from tensorflow.keras.models import Model
+from tensorflow.keras.losses import binary_crossentropy,mse
+
+input_img = Input(shape=(28, 28, 1))
+
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+encoder = Model(input_img,encoded)
+
+input_rep = Input(shape=(4,4,8))
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(input_rep)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+decoder = Model(input_rep,decoded)
+
+autoencoder = Model(input_img, decoder(encoder(input_img)))
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+     
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+     
+
+autoencoder.fit(x_train, x_train,
+                epochs=25,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test, x_test))
